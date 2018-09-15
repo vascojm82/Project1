0d9cc4351d9ea3a865a0dc79e92859d33769d3fb
Luis <hhavocs@yahoo.com>
2018-09-15 14:06:17 -0400

    firebase info

---
+++ assets/JS/firebase.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDhtUruqe6PM_B3BxH38flw1uCbb9D73aI",
+    authDomain: "project1-ecommerce.firebaseapp.com",
+    databaseURL: "https://project1-ecommerce.firebaseio.com",
+    projectId: "project1-ecommerce",
+    storageBucket: "project1-ecommerce.appspot.com",
+    messagingSenderId: "946289156606"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
